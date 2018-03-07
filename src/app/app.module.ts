2ed590281f45871ac9f732d5c0ce22902af4b9b1
INTERGRUPO\Dgelvez <dgelvez@intergrupo.com>
2018-03-07 17:18:25 -0500

    no message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { routing, appRoutingProviders } from './app.routing';
 import { HttpClientModule } from '@angular/common/http';
+// import { AppBootstrapModule } from './app-bootstrap/app-bootstrap.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
